2e27918bda3a9de962b272329b1567e3c905b25b
hwang1017 <hru_wang@me.com>
2020-12-14 00:50:08 -0800

    connect to mongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const dotenv = require('dotenv')
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import products from './data/products.js'
+import connectDB from './config/db.js'
+
 dotenv.config()
 
-const products = require('./data/products')
+connectDB();
 
 const app = express()
 
@@ -21,4 +25,7 @@ app.get('/api/products/:id', (req, res) => {
 })
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+)
