e0143920cc3431eb43a806f6b3b967fe2d05e552
hwang1017 <hru_wang@me.com>
2020-12-26 16:28:26 -0800

    deploy to heroku

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import morgan from 'morgan';
 import path from 'path';
-import colors from 'colors';
-import morgan from 'morgan'
+import colors from 'colors'
 import connectDB from './config/db.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import orderRoutes from './routes/orderRoutes.js';
@@ -17,14 +17,10 @@ connectDB();
 const app = express();
 
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -37,6 +33,18 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 //@Excption Handler - Unknown url
 app.use(errorHandler);
